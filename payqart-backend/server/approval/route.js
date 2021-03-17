83f2e659b5469a0adf92b70633ad5fda9d3ae02c
hello ibk <helloibk@helloibk-MacBook-Pro.local>
2021-03-17 15:47:59 +0100

    lms apis for all path

--- payqart-backend/server/approval/route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { approveCredit, updateDownPayment } from "./controller";
-
-const approvalRouter = express.Router();
-
-approvalRouter.post("/approveCredit", approveCredit);
-approvalRouter.post("/updateDownPayment", updateDownPayment);
-
-export default approvalRouter;
--- payqart/src/hooks/useJoinString/index.js
+++
@@ -1,9 +0,0 @@
-function useJoinString(params) {
-  function getIntials(first, last) {
-    return `${first[0]}.${last[0]}`;
-  }
-
-  return {
-    getIntials,
-  };
-}
